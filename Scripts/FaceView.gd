8d2ffa96c90c1a3e5d8e5c87abdf70afe583ba49
Youxuan Zhao <youxuanzhao2007@outlook.com>
2024-01-26 18:22:08 +0800

    humor_display

--- Scripts/FaceView.gd
+++ Scripts/FaceView.gd
@@ -1,5 +1,13 @@
 extends Node2D
 
+@onready var face : Sprite2D = $Face
+@onready var humorFrame : Sprite2D = $HumorFrame
+@onready var humorCover : Sprite2D = $HumorCover
+@onready var humorUnder : Sprite2D = $HumorUnder
+@onready var emotion : Sprite2D = $Emotion
+
+var percentage:float = 0.0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,7 +16,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	humorCover.position.x = percentage
+	emotion.position.x = percentage
 
 func chuckle():
 	pass
